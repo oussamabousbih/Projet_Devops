bd61ee395f42ee52e8a1c0ea3addadf35f25c46b
saif rabbeh <rabbehs>
2024-10-15 19:58:28 +0100

    generating unit tests

--- src/main/java/tn/esprit/tpfoyer/entity/Universite.java
+++ src/main/java/tn/esprit/tpfoyer/entity/Universite.java
@@ -9,6 +9,7 @@ import lombok.experimental.FieldDefaults;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Universite {
