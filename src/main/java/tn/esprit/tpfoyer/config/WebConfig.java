c9fb13fd745ba4b699144d7bca3123c511e4b023
saif rabbeh <rabbehs>
2024-10-23 01:21:37 +0100

    update Dockerfile

--- src/main/java/tn/esprit/tpfoyer/config/WebConfig.java
+++ src/main/java/tn/esprit/tpfoyer/config/WebConfig.java
@@ -9,13 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${app.api.settings.cross-origin:localhost}")
-    private String[] allowedOrigins;
+    @Value("${app.api.settings.cross-origin.patterns:localhost}")
+    private String[] allowedOriginPatterns;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all paths
-                .allowedOrigins(allowedOrigins) // Use the value from your properties file
+                .allowedOriginPatterns(allowedOriginPatterns)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
                 .allowedHeaders("*")
                 .allowCredentials(true); // Allow credentials if needed
