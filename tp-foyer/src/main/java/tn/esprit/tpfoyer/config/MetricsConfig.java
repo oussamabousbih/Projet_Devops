0051242417042abc6d118c1dc0b9f490d3053818
oussamabousbih <80363581+oussamabousbih@users.noreply.github.com>
2024-10-29 22:10:10 +0100

    Update MetricsConfig.java

--- tp-foyer/src/main/java/tn/esprit/tpfoyer/config/MetricsConfig.java
+++ tp-foyer/src/main/java/tn/esprit/tpfoyer/config/MetricsConfig.java
@@ -1,13 +1,29 @@
+package tn.esprit.tpfoyer.config;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MetricsConfig {
+public class MetricsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public MetricsEndpoint metricsEndpoint(MeterRegistry meterRegistry) {
-        return new MetricsEndpoint(meterRegistry);
+    @Value("${app.api.settings.cross-origin.patterns:localhost}")
+    private String[] allowedOriginPatterns;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all paths
+                .allowedOriginPatterns(allowedOriginPatterns)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
+                .allowedHeaders("*")
+                .allowCredentials(true); // Allow credentials if needed
     }
 }
+
+
+
+
