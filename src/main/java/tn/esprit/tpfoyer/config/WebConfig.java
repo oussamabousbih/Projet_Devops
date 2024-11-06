5b9f34b0339b066482a2a2badb9372a551124c91
oussamabousbih <80363581+oussamabousbih@users.noreply.github.com>
2024-11-06 21:45:53 +0100

    Update WebConfig.java

--- src/main/java/tn/esprit/tpfoyer/config/WebConfig.java
+++ src/main/java/tn/esprit/tpfoyer/config/WebConfig.java
@@ -7,17 +7,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig  {
 
-    @Value("${app.api.settings.cross-origin.patterns:localhost}")
-    private String[] allowedOriginPatterns;
+    // @Value("${app.api.settings.cross-origin.patterns:localhost}")
+    // private String[] allowedOriginPatterns;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply to all paths
-                .allowedOriginPatterns(allowedOriginPatterns)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
-                .allowedHeaders("*")
-                .allowCredentials(true); // Allow credentials if needed
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**") // Apply to all paths
+    //             .allowedOriginPatterns(allowedOriginPatterns)
+    //             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
+    //             .allowedHeaders("*")
+    //             .allowCredentials(true); // Allow credentials if needed
+    // }
 }
